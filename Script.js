5e11e84fb1806e04e5544a9e7a78d145583cb7ca
yikkea <farhasifkhan25@gmail.com>
2026-06-08 17:19:58 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,16 @@
+//the script ,muhahahahahah
+
+function ctheme() {
+  var linkSelector=document.getElementById("link");
+linkSelector.href = "CrazyTheme.css"
+}
+
+function ltheme() {
+  var linkSelector=document.getElementById("link");
+  linkSelector.href="Light theme.css";
+}
+
+function dtheme() {
+  var linkSelector=document.getElementById("link");
+  linkSelector.href="DarkTheme.css";
+}
